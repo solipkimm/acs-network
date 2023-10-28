197af284acf328df938908f0067e1d8882532dcf
EC2 Default User <ec2-user@ip-172-31-49-30.ec2.internal>
2023-10-28 08:37:03 +0000

    remove webservers

--- aws_webservers/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "solip-lab-s3"            
-    key    = "dev/webserver/terraform.tfstate" 
-    region = "us-east-1"            
-  }
-}
--- aws_webservers/install_httpd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum -y update
-yum -y install httpd
-myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1>ACS 730</h1><p>My name is Solip Kim<br><br>My private IP is $myip<br>Built by Terraform!</p>"  >  /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
